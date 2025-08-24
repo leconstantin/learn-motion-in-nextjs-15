9fb7b4c5854798bd116074cbbf34abbcf63715ae
Leo Constantin <lcon69184@gmail.com>
2025-08-24 20:36:34 +0200

    added some shadcn and motion init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ModeSwitcher } from "@/components/custom/mode-switcher";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { META_THEME_COLORS } from "@/hooks/use-metal-color";
 
@@ -53,7 +54,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <nav className="flex justify-end px-6 py-1">
+            <ModeSwitcher />
+          </nav>
+          <main className="px-4 md:px-6 min-h-screen relative">{children}</main>
         </ThemeProvider>
       </body>
     </html>
